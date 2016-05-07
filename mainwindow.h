8096153d0af7793a007768b2593b02869c6fbb78
Titouan Galopin <galopintitouan@gmail.com>
2016-05-08 01:20:02 +0200

    Create main interface

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "ui_mainwindow.h"
 
 namespace Ui {
 class MainWindow;
@@ -12,8 +13,16 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = 0) : QMainWindow(parent), ui(new Ui::MainWindow) {
+        ui->setupUi(this);
+
+        ui->globalLayout->setStretchFactor(ui->sidebarLayout, 2);
+        ui->globalLayout->setStretchFactor(ui->mainLayout, 5);
+    }
+
+    ~MainWindow() {
+        delete ui;
+    }
 
 private:
     Ui::MainWindow *ui;
