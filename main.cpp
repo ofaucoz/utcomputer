dc4668ed04364bdb552554ea24e82d0943e4d7a3
Titouan Galopin <galopintitouan@gmail.com>
2016-05-16 23:47:35 +0200

    Create TokenDumper for debug and implement lexer in UTComputer

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include "lib/utcomputer.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+    AtomLiteralDefinition atomLiteralDefinition;
+    WhitespaceLiteralDefinition whitespaceLiteralDefinition;
+    NumericLiteralDefinition numLiteralDefinition;
+    OperatorLiteralDefinition operatorLiteralDefinition;
+    ExpressionLiteralDefinition expressionLiteralDefinition;
+    ProgramLiteralDefinition programLiteralDefinition;
+
+    Lexer lexer;
+    lexer.addDefinition(whitespaceLiteralDefinition);
+    lexer.addDefinition(numLiteralDefinition);
+    lexer.addDefinition(operatorLiteralDefinition);
+    lexer.addDefinition(expressionLiteralDefinition);
+    lexer.addDefinition(programLiteralDefinition);
+    lexer.addDefinition(atomLiteralDefinition);
+
+    Resolver resolver;
+
+    Runner runner;
+
+    UTComputer computer(lexer, resolver, runner);
+
+    computer.execute("FOO 36 -2/4 6$4 98/2$-55/4 + - * / EVAL '3+3*56' [1 +]");
+
     return 0;
 }
