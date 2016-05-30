552c3385ca0fc841ba24d7a1feca375f7e589cc1
Titouan Galopin <galopintitouan@gmail.com>
2016-05-30 17:54:49 +0200

    Implement Numerator/Denominator operator

---
+++ lib/operator/denominator.cpp
@@ -0,0 +1,22 @@
+#include "denominator.h"
+
+void DenominatorOperator::apply(LiteralsStack &stack) const {
+    if (stack.size() < 1) {
+        throw InvalidSyntaxException("Denominator operator requires 1 operand");
+    }
+
+    LiteralPointer first = stack.top();
+    stack.pop();
+
+    NumericLiteralPointer firstNumeric = dynamic_pointer_cast<NumericLiteral>(first);
+
+    if (! firstNumeric) {
+        throw InvalidOperandException(first->toString());
+    }
+
+    if (firstNumeric->toString().find("$") != std::string::npos) {
+        throw InvalidOperandException(first->toString());
+    }
+
+    stack.pushAndNotify(LiteralPointer(new NumericLiteral(firstNumeric->getRealDenominator())));
+}
